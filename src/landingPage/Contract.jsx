d9f3f862737eb982ef477a25fd35a02fa97152ed
Abhay Pratap Singh <abhayalternativeid@gmail.com>
2025-09-03 22:35:58 +0530

    fixed the readability issue

--- src/landingPage/Contract.jsx
+++ src/landingPage/Contract.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Contract = () => {
   return (
     <div className="flex flex-col text-center p-4 m-4">
-      <div className="text-3xl font-bold text-gray-800 mb-4">
-        <h1>What are you looking for?</h1>
+      <div className="text-3xl font-bold text-white-800 mb-4">
+        <h1>What are  you looking for?</h1>
       </div>
       <div className="flex flex-row p-6 m-4">
         <input
@@ -12,7 +12,7 @@ const Contract = () => {
           name="Search"
           id=""
           placeholder="Type here..."
-          className="w-5/6 border border-stone-500 p-4 rounded-full"
+          className="w-5/6 border border-stone-500 p-4 rounded-full text-2xl placeholder-white"
         />
         <button
           type="button"
